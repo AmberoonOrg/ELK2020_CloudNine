e6e3d92ff0e9948ffd02626a04e09afbeaf92d03
Abhilash <bolla.abhilash@amberoon.com>
2020-08-25 19:09:14 +0530

    changed wget to curl

--- python_scripts/alephdata/automate_download.sh
+++ python_scripts/alephdata/automate_download.sh
@@ -1,11 +1,11 @@
-wget https://storage.googleapis.com/occrp-data-exports/us_cia_world_leaders/us_cia_world_leaders.json
-wget https://storage.googleapis.com/occrp-data-exports/everypolitician/everypolitician.json
-wget https://storage.googleapis.com/occrp-data-exports/eu_meps/eu_meps.json
-wget https://storage.googleapis.com/occrp-data-exports/un_sc_sanctions/un_sc_sanctions.json
-wget https://storage.googleapis.com/occrp-data-exports/ch_seco_sanctions/ch_seco_sanctions.json
-wget https://storage.googleapis.com/occrp-data-exports/us_ofac/us_ofac.json
-wget https://storage.googleapis.com/occrp-data-exports/us_bis_denied/us_bis_denied.json
-wget https://storage.googleapis.com/occrp-data-exports/gb_hmt_sanctions/gb_hmt_sanctions.json
-wget https://storage.googleapis.com/occrp-data-exports/eu_eeas_sanctions/eu_eeas_sanctions.json
-wget https://storage.googleapis.com/occrp-data-exports/au_dfat_sanctions/au_dfat_sanctions.json
-wget https://storage.googleapis.com/occrp-data-exports/ca_dfatd_sema_sanctions/ca_dfatd_sema_sanctions.json
+curl -O https://storage.googleapis.com/occrp-data-exports/us_cia_world_leaders/us_cia_world_leaders.json
+curl -O https://storage.googleapis.com/occrp-data-exports/everypolitician/everypolitician.json
+curl -O https://storage.googleapis.com/occrp-data-exports/eu_meps/eu_meps.json
+curl -O https://storage.googleapis.com/occrp-data-exports/un_sc_sanctions/un_sc_sanctions.json
+curl -O https://storage.googleapis.com/occrp-data-exports/ch_seco_sanctions/ch_seco_sanctions.json
+curl -O https://storage.googleapis.com/occrp-data-exports/us_ofac/us_ofac.json
+curl -O https://storage.googleapis.com/occrp-data-exports/us_bis_denied/us_bis_denied.json
+curl -O https://storage.googleapis.com/occrp-data-exports/gb_hmt_sanctions/gb_hmt_sanctions.json
+curl -O https://storage.googleapis.com/occrp-data-exports/eu_eeas_sanctions/eu_eeas_sanctions.json
+curl -O https://storage.googleapis.com/occrp-data-exports/au_dfat_sanctions/au_dfat_sanctions.json
+curl -O https://storage.googleapis.com/occrp-data-exports/ca_dfatd_sema_sanctions/ca_dfatd_sema_sanctions.json
